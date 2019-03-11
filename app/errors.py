fce8e0b87a6739eb6f2ce044d7bb27fc7a162a90
Imipenem <philipp_ehm@protonmail.com>
2019-03-11 21:58:11 +0100

    Chapter 7 Error handling
    - adding a error handler for 404/500 and formatting its template
    
    - enable email error notfications

---
+++ app/errors.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import app, db
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()  # ensure that failed db session don´t interfere with regular db accesses
+    return render_template('500.html'), 500
+
